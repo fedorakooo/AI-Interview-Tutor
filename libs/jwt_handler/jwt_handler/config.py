91d6b52063184b1b79b1a259eec1b1d0ca252ad1
Aliaksandr Fedaraka <fedorakooo@gmail.com>
2025-08-06 16:46:22 +0200

    refactor(jwt-handler): Remove private key fields and update configuration

--- libs/jwt_handler/jwt_handler/config.py
+++ libs/jwt_handler/jwt_handler/config.py
@@ -2,19 +2,15 @@ from pathlib import Path
 from typing import Any
 
 import yaml
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class JWTSettings(BaseSettings):
     """JWT settings."""
 
-    PRIVATE_KEY: str
-    PUBLIC_KEY: str
     algorithm: str = "RS256"
     access_token_expire_minutes: float = 10
     refresh_token_expire_minutes: float = 20160
-
-    model_config = SettingsConfigDict(env_file="../.env", extra="ignore")
 
     @classmethod
     def load_from_yaml(cls, file_path: str = "config.yaml") -> dict[str, Any]:
